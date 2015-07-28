64f14b82973b58ce963712c0a1e495a937e6ef62
Christian Hoogerheyde <choogerheyde@gmail.com>
2015-07-27 23:29:03 -0400

    jsFiddle code for returning selected text
    http://jsfiddle.net/PQbb7/7/

--- popup.js
+++ popup.js
@@ -1,1 +1,48 @@
-#popup over <p>
+function getSelected() {
+    if (window.getSelection) {
+        return window.getSelection();
+    }
+    else if (document.getSelection) {
+        return document.getSelection();
+    }
+    else {
+        var selection = document.selection && document.selection.createRange();
+        if (selection.text) {
+            return selection.text;
+        }
+        return false;
+    }
+    return false;
+}
+
+/*this section is using jQuery**/
+$('#body').mouseup(function() {
+    var selection = getSelected();
+
+    if (selection) {
+        /*$('div#pop-up').show()*/
+        alert(selection);
+    }
+});
+
+/*<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+    <script type="text/javascript">
+      $(function() {
+        var moveLeft = 20;
+        var moveDown = 10;
+        
+        $('a#trigger').hover(function(e) {
+          $('div#pop-up').show();
+          //.css('top', e.pageY + moveDown)
+          //.css('left', e.pageX + moveLeft)
+          //.appendTo('body');
+        }, function() {
+          $('div#pop-up').hide();
+        });
+        
+        $('a#trigger').mousemove(function(e) {
+          $("div#pop-up").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+        });
+        
+      });
+    </script>*/
